d53af7c0aca77666275566bc8afe38fa6496a375
Keventon <kevertonguimaraes19@gmail.com>
2026-01-27 19:44:35 -0300

    adicionado host no server.ts para enviar para produção

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { app } from "./app.ts";
 import { env } from "./env/index.ts";
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log("HTTP Server is running on http://localhost:3333");
-});
+app
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log("HTTP Server is running!");
+  });
